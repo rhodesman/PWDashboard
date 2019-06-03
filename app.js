51d6c053aed2403fc3afe0fdb4639067f2842376
Jason Rhodes <jrhodes@catalystlabs.co>
2019-06-03 17:26:22 -0400

    added in logic to get computer ip address upon start.

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 // app.js
+
+'use strict';
+
+const os = require('os');
+const ifaces = os.networkInterfaces();
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const path = require('path');
-
 const app = express();
 const port = process.env.PORT || 3301;
+
+var thisIPaddr;
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+      thisIPaddr = thisIPaddr + ',' + alias, iface.address + ':' + port;
+    } else {
+      // this interface has only one ipv4 adress
+      //console.log(ifname, iface.address);
+      thisIPaddr = iface.address + ':' + port;
+    }
+    ++alias;
+  });
+});
 
 app.use(function (req, res, next) {
     //Allow Any system to access your API (change this to whatever you need)
@@ -31,5 +59,5 @@ app.use(function (req, res) {
 
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://${thisIPaddr}`);
 });
